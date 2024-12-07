baab2fb7a59930615f3e40c4c3ff3165d744c1f7
Philix27 <philixbob@gmail.com>
2024-12-07 13:06:18 +0100

    chore: install vercel analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       {children}
+      <Analytics />
     </ThemeProvider>
   );
 }
